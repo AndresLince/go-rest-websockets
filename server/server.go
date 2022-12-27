7e7fe6fa707b2234f786c74aef9026b739c93d4a
AndresLince <lynxdarmatron@gmail.com>
2022-12-27 06:52:22 -0500

    Change port for addr

--- server/server.go
+++ server/server.go
@@ -10,7 +10,7 @@ import (
 )
 
 type Config struct {
-	Port        string
+	Addr        string
 	JWTSecret   string
 	DatabaseUrl string
 }
@@ -29,8 +29,8 @@ func (b *Broker) Config() *Config {
 }
 
 func NewServer(ctx context.Context, config *Config) (*Broker, error) {
-	if config.Port == "" {
-		return nil, errors.New("port is required")
+	if config.Addr == "" {
+		return nil, errors.New("addr is required")
 	}
 	if config.JWTSecret == "" {
 		return nil, errors.New("JWTSecret is required")
@@ -48,8 +48,8 @@ func NewServer(ctx context.Context, config *Config) (*Broker, error) {
 func (b *Broker) Start(binder func(s Server, r *mux.Router)) {
 	b.router = mux.NewRouter()
 	binder(b, b.router)
-	log.Println("Starting server on port", b.config.Port)
-	err := http.ListenAndServe(b.config.Port, b.router)
+	log.Println("Starting server on addr", b.config.Addr)
+	err := http.ListenAndServe(b.config.Addr, b.router)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
