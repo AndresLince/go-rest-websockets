5c06649cd3719419beca605cbfb50c2dfbac77f6
AndresLince <lynxdarmatron@gmail.com>
2022-12-27 06:53:21 -0500

    Add first endpoint

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/AndresLince/go-rest-websockets/handlers"
+	"github.com/AndresLince/go-rest-websockets/server"
+	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	ADDR := os.Getenv("ADDR")
+	JWT_SECRET := os.Getenv("JWT_SECRET")
+	DATABASE_URL := os.Getenv("DATABASE_URL")
+	s, err := server.NewServer(context.Background(), &server.Config{
+		JWTSecret:   JWT_SECRET,
+		Addr:        ADDR,
+		DatabaseUrl: DATABASE_URL,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	s.Start(BindRoutes)
+}
+
+func BindRoutes(s server.Server, r *mux.Router) {
+	r.HandleFunc("/", handlers.HomeHandler(s)).Methods(http.MethodGet)
+}
