f19ce24430f19e512e214d7b5c7463872b47848d
AndresLince <lynxdarmatron@gmail.com>
2023-01-31 19:49:38 -0500

    Remove extra comma in up.sql

--- database/up.sql
+++ database/up.sql
@@ -4,5 +4,5 @@ CREATE TABLE users(
     id VARCHAR(32) PRIMARY KEY,
     password VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
-    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    created_at TIMESTAMP NOT NULL DEFAULT NOW()
 )
