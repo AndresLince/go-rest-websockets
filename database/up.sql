152decd74667a7204f6251aeb5d5134b1f13a1c2
AndresLince <lynxdarmatron@gmail.com>
2023-02-11 08:55:16 -0500

    Fix database script error

--- database/up.sql
+++ database/up.sql
@@ -5,7 +5,7 @@ CREATE TABLE users(
     password VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE,
     created_at TIMESTAMP NOT NULL DEFAULT NOW()
-)
+);
 
 DROP TABLE IF EXISTS posts;
 
@@ -15,4 +15,4 @@ CREATE TABLE posts(
     created_at TIMESTAMP NOT NULL DEFAULT NOW(),
     user_id VARCHAR(32) NOT NULL,
     FOREIGN KEY (user_id) REFERENCES users(id)
-)
+);
