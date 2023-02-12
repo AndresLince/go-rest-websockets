4de126270f32fe36a2bf26c2c7ae234f682b17e0
AndresLince <lynxdarmatron@gmail.com>
2023-02-12 12:08:29 -0500

    Add delete post handler

--- handlers/post.go
+++ handlers/post.go
@@ -117,3 +117,43 @@ func UpdatePostHandler(s server.Server) http.HandlerFunc {
 		}
 	}
 }
+
+func DeletePostHandler(s server.Server) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		params := mux.Vars(r)
+		tokenString := strings.TrimSpace(r.Header.Get("Authorization"))
+		token, err := jwt.ParseWithClaims(tokenString, &models.AppClaims{}, func(token *jwt.Token) (interface{}, error) {
+			return []byte(s.Config().JWTSecret), nil
+		})
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusUnauthorized)
+			return
+		}
+		if claims, ok := token.Claims.(*models.AppClaims); ok && token.Valid {
+			post, err := repository.GetPostById(r.Context(), params["id"])
+
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+
+			if len(post.Id) == 0 {
+				http.Error(w, "Bad request", http.StatusBadRequest)
+				return
+			}
+
+			err = repository.DeletePost(r.Context(), params["id"], claims.UserId)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+
+			w.Header().Set("Content-Type", "application/json")
+			json.NewEncoder(w).Encode(PostUpdateResponse{
+				Message: "Post Deleted",
+			})
+		} else {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	}
+}
